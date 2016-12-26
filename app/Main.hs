6951449a5ef604cc2ba52aee9c4cca7122dae75a
mcnestrb <brianmcnestry@gmail.com>
2016-12-26 20:36:10 +0000

    add new run monad transformer and set function

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,22 @@
 module Main where
 
+--import Wonderland
 import Wonderland
 import System.Environment
 
 main :: IO ()
 main = do
-         args <- getArgs
-         content <- readFile (args !! 0)
-         let linesOfFile = lines content
-         printOutAllTheStrings linesOfFile
+          f <- getLine
+          x <- readFile f
+          let newStats = getStats x
+          mapM_ putStrLn (map (show) newStats)
 
-printOutAllTheStrings :: [String] -> IO ()
-printOutAllTheStrings [x] = putStrLn x
-printOutAllTheStrings (x:xs) = do putStrLn x
-                                  printOutAllTheStrings xs
+getStats :: String -> [Statement]
+getStats x = putStatementsInList (read x)
+{-
+doThePrintThing :: IO ()
+doThePrintThing = putStrLn $ show (Seq (Assign "varX" (Const (I 5))) (Assign "varX" ((Add (Var "varX") (Const (I 2))))) )
+-}
+putStatementsInList :: Statement -> [Statement]
+putStatementsInList (Seq stat stats) = stat : (putStatementsInList stats)
+putStatementsInList stat = [stat]
