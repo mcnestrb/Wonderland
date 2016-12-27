4011e0d57623d9f22fc2fc48d793b0175461ed5c
mcnestrb <brianmcnestry@gmail.com>
2016-12-27 17:25:48 +0000

    stepping and executing with assign and print

--- app/Main.hs
+++ app/Main.hs
@@ -5,18 +5,14 @@ import Wonderland
 import System.Environment
 
 main :: IO ()
-main = do
-          f <- getLine
+main = do f <- getLine
           x <- readFile f
           let newStats = getStats x
-          mapM_ putStrLn (map (show) newStats)
+          run newStats
 
 getStats :: String -> [Statement]
 getStats x = putStatementsInList (read x)
-{-
-doThePrintThing :: IO ()
-doThePrintThing = putStrLn $ show (Seq (Assign "varX" (Const (I 5))) (Assign "varX" ((Add (Var "varX") (Const (I 2))))) )
--}
+
 putStatementsInList :: Statement -> [Statement]
 putStatementsInList (Seq stat stats) = stat : (putStatementsInList stats)
 putStatementsInList stat = [stat]
--- app/Wonderland.hs
+++ app/Wonderland.hs
@@ -5,7 +5,7 @@
 -- I want my own definition of lookup and I want to write my own function
 -- named "print".
 module Wonderland (
-Statement(..), Expr(..), Val(..)
+run, Statement(..)
 ) where
 import Prelude hiding (lookup, print)
 
@@ -39,18 +39,17 @@ data Expr = Const Val
      deriving (Eq, Show, Read)
 
 type Name = String
-type EvalEnv = Map.Map Name Val
 type Env = Map.Map Name [Val]
 
 lookup k t = case Map.lookup k t of
-                Just x -> return x
+                Just x -> return $ head x
                 Nothing -> fail ("Unknown variable "++k)
 
 -- {-- Monadic style expression evaluator,
 --  -- with error handling and Reader monad instance to carry dictionary
 --  --}
 
-type Eval a = ReaderT EvalEnv (ExceptT String Identity) a
+type Eval a = ReaderT Env (ExceptT String Identity) a
 runEval env ex = runIdentity ( runExceptT ( runReaderT ex env) )
 
 -- This evaluator could be a little neater
@@ -116,18 +115,51 @@ data Statement = Assign String Expr
                  deriving (Eq, Show, Read)
 
 
-type Run a = StateT Env IO a
-runRun p = runStateT p Map.empty
+type Run a = StateT Env (ExceptT String IO) a
+runRun p = runExceptT (runStateT p Map.empty)
 
 set :: Name -> Val -> Run ()
 set n v = do st <- get
-             let maybeVals = Map.lookup n st
-             case maybeVals of
+             case Map.lookup n st of
                Nothing -> state $ (\table -> ((), Map.insert n [v] table))
                Just maybeVals -> state $ (\table -> ((), Map.insert n (v:maybeVals) table))
-{-
+
 exec :: Statement -> Run ()
 exec (Assign s v) = do st <- get
                        Right val <- return $ runEval st (eval v)
-                       set s val
+                       liftIO $ System.print $ "Next line to execute: " ++ show (Assign s v)
+                       letter <- liftIO $ getLine
+                       case letter of
+                         "n" -> set s val
+                         _   -> do liftIO $ System.print $ "Not a valid input - press n to step forward"
+                                   exec (Assign s v)
+
+exec (Print e) = do st <- get
+                    Right val <- return $ runEval st (eval e)
+                    liftIO $ System.print $ "Next line to execute: " ++ show (Print e)
+                    letter <- liftIO $ getLine
+                    case letter of
+                      "n" -> do liftIO $ System.print val
+                                return ()
+                      _   -> do liftIO $ System.print $ "Not a valid input - press n to step forward"
+                                exec (Print e)
+{-
+exec (If cond s0 s1) = do st <- get
+                          Right (B val) <- return $ runEval st (eval cond)
+													liftIO $ System.print $ show (If cond s0 s1)
+													letter <- liftIO $ getLine
+													case letter of
+														"n" -> do liftIO $ if val then do exec s0 else do exec s1
+														_   -> do liftIO $ System.print $ "Not a valid input - press n to step forward"
+			                                exec (If cond s0 s1)
 -}
+step :: [Statement] -> Run ()
+step [] = return ()
+step (stat:stats) = do exec stat
+                       step stats
+
+run :: [Statement] -> IO ()
+run stats = do result <- runExceptT $ (runStateT $ step stats) Map.empty
+               case result of
+                 Right ((), env) -> return ()
+                 Left exn -> System.print ("Uncaught exception " ++ exn)
